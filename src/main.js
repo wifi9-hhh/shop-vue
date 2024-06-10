4247c80e9935941f53de3339da5c01351dd2e5ec
unknown <735604979@qq.com>
2024-06-10 14:05:22 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import Router from './tools/Router'
+import Store from './tools/Storage'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css';
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(Router)
+app.use(Store)
+app.mount('#app')
+
